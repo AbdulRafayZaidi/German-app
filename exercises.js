84d60945e5639dfd9e0333b8ca3d2c74e241ac78
Abdul Rafay <72885508+AbdulRafayZaidi@users.noreply.github.com>
2026-01-14 17:05:31 +0100

    Create exercises.js

---
+++ exercises.js
@@ -0,0 +1,33 @@
+
+const exercises = [
+    { 
+        sentence: "Der alt__ Mann schläft.", 
+        ending: "e", 
+        context: "Nom / Masc / Definite" 
+    },
+    { 
+        sentence: "Ich habe einen rot__ Apfel.", 
+        ending: "en", 
+        context: "Acc / Masc / Indefinite" 
+    },
+    { 
+        sentence: "Das ist ein klein__ Haus.", 
+        ending: "es", 
+        context: "Nom / Neut / Indefinite" 
+    },
+    { 
+        sentence: "Ich helfe der krank__ Frau.", 
+        ending: "en", 
+        context: "Dat / Fem / Definite" 
+    },
+    { 
+        sentence: "Wir essen mit scharf__ Senf.", 
+        ending: "em", 
+        context: "Dat / Masc / No Article" 
+    },
+    { 
+        sentence: "Wegen des schlecht__ Wetters bleiben wir hier.", 
+        ending: "en", 
+        context: "Gen / Neut / Definite" 
+    }
+];
